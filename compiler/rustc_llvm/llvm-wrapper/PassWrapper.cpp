5e774e41b7ef205703912d35a64dae5c1f21d4b7
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2022-10-24 10:52:40 -0400

    [SOL] switch to new SBF LLVM backend

--- compiler/rustc_llvm/llvm-wrapper/PassWrapper.cpp
+++ compiler/rustc_llvm/llvm-wrapper/PassWrapper.cpp
@@ -158,6 +158,12 @@ extern "C" void LLVMTimeTraceProfilerFinish(const char* FileName) {
 #define SUBTARGET_BPF
 #endif
 
+#ifdef LLVM_COMPONENT_SBF
+#define SUBTARGET_SBF SUBTARGET(SBF)
+#else
+#define SUBTARGET_SBF
+#endif
+
 #define GEN_SUBTARGETS                                                         \
   SUBTARGET_X86                                                                \
   SUBTARGET_ARM                                                                \
@@ -173,6 +179,7 @@ extern "C" void LLVMTimeTraceProfilerFinish(const char* FileName) {
   SUBTARGET_RISCV                                                              \
   SUBTARGET_LOONGARCH                                                          \
   SUBTARGET_BPF                                                                \
+  SUBTARGET_SBF                                                                \
 
 #define SUBTARGET(x)                                                           \
   namespace llvm {                                                             \
